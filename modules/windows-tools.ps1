f20cbefa7671e47afb4ed0a7d452ea23823e003e
Banliecomp <ultimatephone88@gmail.com>
2026-03-03 18:41:25 +0700

    Update windows-tools.ps1

--- modules/windows-tools.ps1
+++ modules/windows-tools.ps1
@@ -7,6 +7,7 @@ function Show-WindowsTools {
         Write-Host "1. System Information"
         Write-Host "2. Disk Cleanup"
         Write-Host "3. Check Disk (C:)"
+        Write-Host "4. Keyboard Tester Portable (C:)"
         Write-Host "0. Back"
         Write-Host ""
 
@@ -29,6 +30,17 @@ function Show-WindowsTools {
                 Pause
             }
 
+            "4" {
+            $url = "LINK_DOWNLOAD_EXE_KAMU"
+            $path = "$env:TEMP\keyboardtester.exe"
+            if (-not (Test-Path $path)) {
+            Write-Host "Downloading Keyboard Tester..."
+            irm $url -OutFile $path
+            }
+            Start-Process $path
+            Pause
+            }
+            
             "0" { return }
         }
     }
