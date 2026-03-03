2b0d207181442932ee03fd98588d083b5ecf73ca
Banliecomp <ultimatephone88@gmail.com>
2026-03-03 18:02:19 +0700

    Refactor tools menu to improve user options

--- modules/tools.ps1
+++ modules/tools.ps1
@@ -1,19 +1,18 @@
-function Menu-Tools {
-    Clear-Host
-    Write-Host "=== TOOLS WINDOWS ==="
-    Write-Host "1. System Info"
-    Write-Host "2. Scan SFC"
-    Write-Host "0. Kembali"
-    $c = Read-Host "Pilih"
+function Show-WindowsTools {
+    while ($true) {
+        Clear-Host
+        Write-Host "==== WINDOWS TOOLS ====" -ForegroundColor Yellow
+        Write-Host "1. System Information"
+        Write-Host "2. Disk Cleanup"
+        Write-Host "0. Back"
+        Write-Host ""
 
-    switch ($c) {
-        "1" { 
-            systeminfo
-            Pause
-        }
-        "2" { 
-            sfc /scannow
-            Pause
+        $choice = Read-Host "Select"
+
+        switch ($choice) {
+            "1" { systeminfo; Pause }
+            "2" { cleanmgr; Pause }
+            "0" { return }
         }
     }
 }
