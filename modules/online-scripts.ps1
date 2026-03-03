d5e13360249b4844835c9e5fea1eeb8abe850a6c
Banliecomp <ultimatephone88@gmail.com>
2026-03-03 18:05:29 +0700

    Add Show-OnlineScripts function to manage scripts

---
+++ modules/online-scripts.ps1
@@ -0,0 +1,34 @@
+function Show-OnlineScripts {
+
+    $scripts = @{
+        "1" = @{
+            Name = "Microsoft Activation Script"
+            Url  = "https://get.activated.win"
+        }
+        "2" = @{
+            Name = "Example Script"
+            Url  = "https://example.com/script.ps1"
+        }
+    }
+
+    while ($true) {
+        Clear-Host
+        Write-Host "==== ONLINE SCRIPTS ====" -ForegroundColor Cyan
+
+        foreach ($key in $scripts.Keys) {
+            Write-Host "$key. $($scripts[$key].Name)"
+        }
+
+        Write-Host "0. Back"
+        Write-Host ""
+
+        $choice = Read-Host "Select"
+
+        if ($choice -eq "0") { return }
+
+        if ($scripts.ContainsKey($choice)) {
+            irm $scripts[$choice].Url | iex
+            Pause
+        }
+    }
+}
