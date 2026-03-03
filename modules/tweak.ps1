edffc57d605ad9cb7b53c244bb638262d91de13a
Banliecomp <ultimatephone88@gmail.com>
2026-03-03 17:36:40 +0700

    Implement Menu-Tweak function for system tweaks
    Added Menu-Tweak function to provide system tweaking options.

--- modules/tweak.ps1
+++ modules/tweak.ps1
@@ -1,1 +1,34 @@
+function Menu-Tweak {
+    Clear-Host
+    Write-Host "=== TWEAK SYSTEM ==="
+    Write-Host "1. Show File Extension"
+    Write-Host "0. Kembali"
+    $c = Read-Host "Pilih"
 
+    switch ($c) {
+        "1" {
+            Set-ItemProperty `
+            -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" `
+            -Name "HideFileExt" -Value 0
+        }
+    }
+}
+
+function Show-MainMenu {
+    do {
+        Clear-Host
+        Write-Host "==== INSTALL CENTER ===="
+        Write-Host "1. Tools Windows"
+        Write-Host "2. Install Aplikasi"
+        Write-Host "3. Tweak System"
+        Write-Host "0. Keluar"
+        $m = Read-Host "Pilih"
+
+        switch ($m) {
+            "1" { Menu-Tools }
+            "2" { Menu-Install }
+            "3" { Menu-Tweak }
+        }
+
+    } while ($m -ne "0")
+}
