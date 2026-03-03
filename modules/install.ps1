fddd45cbbc533d77f913d0430cfd3599147184ae
Banliecomp <ultimatephone88@gmail.com>
2026-03-03 18:06:18 +0700

    Refactor installation functions and update menu

--- modules/install.ps1
+++ modules/install.ps1
@@ -1,41 +1,24 @@
-function Install-Chrome {
-    Write-Log "Install Chrome"
-    if (Get-Command winget -ErrorAction SilentlyContinue) {
-        winget install Google.Chrome -e --accept-package-agreements --accept-source-agreements
-    } else {
-        $url = "https://dl.google.com/chrome/install/latest/chrome_installer.exe"
-        $file = "$env:TEMP\chrome.exe"
-        Invoke-WebRequest $url -OutFile $file
-        Start-Process $file -ArgumentList "/silent /install" -Wait
-    }
-    Pause
-}
+function Show-Apps {
+    while ($true) {
+        Clear-Host
+        Write-Host "==== APPLICATIONS ====" -ForegroundColor Green
+        Write-Host "1. Install Google Chrome"
+        Write-Host "2. Install 7zip"
+        Write-Host "0. Back"
+        Write-Host ""
 
-function Install-VLC {
-    Write-Log "Install VLC"
-    if (Get-Command winget -ErrorAction SilentlyContinue) {
-        winget install VideoLAN.VLC -e
-    } else {
-        $url = "https://get.videolan.org/vlc/last/win64/vlc-setup.exe"
-        $file = "$env:TEMP\vlc.exe"
-        Invoke-WebRequest $url -OutFile $file
-        Start-Process $file -ArgumentList "/S" -Wait
-    }
-    Pause
-}
+        $choice = Read-Host "Select"
 
-function Menu-Install {
-    Clear-Host
-    Write-Host "=== INSTALL APLIKASI ==="
-    Write-Host "1. Google Chrome"
-    Write-Host "2. VLC"
-    Write-Host "3. Install Semua"
-    Write-Host "0. Kembali"
-    $c = Read-Host "Pilih"
-
-    switch ($c) {
-        "1" { Install-Chrome }
-        "2" { Install-VLC }
-        "3" { Install-Chrome; Install-VLC }
+        switch ($choice) {
+            "1" {
+                winget install Google.Chrome -e
+                Pause
+            }
+            "2" {
+                winget install 7zip.7zip -e
+                Pause
+            }
+            "0" { return }
+        }
     }
 }
