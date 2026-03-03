5db75f02549b5bb88b3d04e042804c47b415ad7f
Banliecomp <ultimatephone88@gmail.com>
2026-03-03 18:10:35 +0700

    Update apps.ps1

--- modules/apps.ps1
+++ modules/apps.ps1
@@ -1,23 +1,34 @@
 function Show-Apps {
+
     while ($true) {
+
         Clear-Host
         Write-Host "==== APPLICATIONS ====" -ForegroundColor Green
         Write-Host "1. Install Google Chrome"
         Write-Host "2. Install 7zip"
+        Write-Host "3. Install VLC"
         Write-Host "0. Back"
         Write-Host ""
 
         $choice = Read-Host "Select"
 
         switch ($choice) {
+
             "1" {
-                winget install Google.Chrome -e
+                winget install Google.Chrome -e --silent
                 Pause
             }
+
             "2" {
-                winget install 7zip.7zip -e
+                winget install 7zip.7zip -e --silent
                 Pause
             }
+
+            "3" {
+                winget install VideoLAN.VLC -e --silent
+                Pause
+            }
+
             "0" { return }
         }
     }
