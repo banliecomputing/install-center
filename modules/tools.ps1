7e5cac5becc6f029142b99bd1a219e3c904241a4
Banliecomp <ultimatephone88@gmail.com>
2026-03-03 17:36:12 +0700

    Update tools.ps1

--- modules/tools.ps1
+++ modules/tools.ps1
@@ -1,1 +1,13 @@
+function Menu-Tools {
+    Clear-Host
+    Write-Host "=== TOOLS WINDOWS ==="
+    Write-Host "1. System Info"
+    Write-Host "2. Scan SFC"
+    Write-Host "0. Kembali"
+    $c = Read-Host "Pilih"
 
+    switch ($c) {
+        "1" { systeminfo }
+        "2" { sfc /scannow }
+    }
+}
