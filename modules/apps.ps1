6e2a23eb68804bddfd1e374b421279911d468549
Banliecomp <ultimatephone88@gmail.com>
2026-03-06 16:44:06 +0700

    Update apps.ps1

--- modules/apps.ps1
+++ modules/apps.ps1
@@ -4,18 +4,18 @@ function Show-Apps {
 
         Clear-Host
         Write-Host "==== APPLICATIONS ====" -ForegroundColor Green
-        Write-Host "==== Browser Windows ====" 
+        Write-Host "==== Browser Windows ====" -ForegroundColor Yellow
         Write-Host "1. Install Google Chrome"
         Write-Host "2. Mozilla Firefox"
         Write-Host ""
-        Write-Host "==== Media Player ====" -ForegroundColor Green
+        Write-Host "==== Tools ====" -ForegroundColor Yellow
         Write-Host "3. Install 7zip"
-        Write-Host "4. Install VLC"
-        Write-Host "5. Acrobat Reader"
-        Write-Host "6. WinRAR LAB"
+        Write-Host "4. Acrobat Reader"
+        Write-Host "5. WinRAR LAB"
         Write-Host ""
-        Write-Host "7. Aimp Player"
-        Write-Host "8. Google Chrome"
+        Write-Host "==== Media Viewer ====" -ForegroundColor Yellow
+        Write-Host "6. Aimp Player"
+        Write-Host "7. Install VLC"
         Write-Host "0. Back"
         Write-Host ""
 
@@ -38,33 +38,27 @@ function Show-Apps {
                 Pause
             }
 
-            "4" {
-                winget install VideoLAN.VLC -e --silent
-                Pause
-            }
 
-            "5" {
+            "4" {
                 winget install Adobe.Acrobat.Reader.64-bit -e --id --silent
                 Pause
             }
             
-            "6" {
+            "5" {
                 winget install RARLab.WinRAR -e --id
                 Pause
             }
             
-            "7" {
+            "6" {
                  winget install -e --id AIMP.AIMP
                 Pause
-            }       
-
-
-            "8" {
-                 winget install -e --id Google.Chrome
-                Pause
-            }    
-
+            } 
             
+            "7" {
+                winget install VideoLAN.VLC -e --silent
+                Pause
+            }
+           
             "0" { return }
         }
     }
