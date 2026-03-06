07950f2c8974a8a0ae07f837b1434d686cf64351
Banliecomp <ultimatephone88@gmail.com>
2026-03-06 16:32:54 +0700

    Update apps.ps1

--- modules/apps.ps1
+++ modules/apps.ps1
@@ -4,12 +4,18 @@ function Show-Apps {
 
         Clear-Host
         Write-Host "==== APPLICATIONS ====" -ForegroundColor Green
+        Write-Host "==== Browser Windows ====" 
         Write-Host "1. Install Google Chrome"
-        Write-Host "2. Install 7zip"
-        Write-Host "3. Install VLC"
-        Write-Host "4. Acrobat Reader"
-        Write-Host "5. WinRAR LAB"
-        Write-Host "6. Aimp Player"
+        Write-Host "2. Mozilla Firefox"
+        Write-Host ""
+        Write-Host "==== Media Player ====" -ForegroundColor Green
+        Write-Host "3. Install 7zip"
+        Write-Host "4. Install VLC"
+        Write-Host "5. Acrobat Reader"
+        Write-Host "6. WinRAR LAB"
+        Write-Host ""
+        Write-Host "7. Aimp Player"
+        Write-Host "8. Google Chrome"
         Write-Host "0. Back"
         Write-Host ""
 
@@ -21,31 +27,43 @@ function Show-Apps {
                 winget install Google.Chrome -e --silent
                 Pause
             }
-
+            
             "2" {
+                 winget install -e --id Mozilla.Firefox
+                Pause
+            }   
+
+            "3" {
                 winget install 7zip.7zip -e --silent
                 Pause
             }
 
-            "3" {
+            "4" {
                 winget install VideoLAN.VLC -e --silent
                 Pause
             }
 
-            "4" {
+            "5" {
                 winget install Adobe.Acrobat.Reader.64-bit -e --id --silent
                 Pause
             }
             
-            "5" {
+            "6" {
                 winget install RARLab.WinRAR -e --id
                 Pause
             }
             
-            "6" {
+            "7" {
                  winget install -e --id AIMP.AIMP
                 Pause
             }       
+
+
+            "8" {
+                 winget install -e --id Google.Chrome
+                Pause
+            }    
+
             
             "0" { return }
         }
